782e42a9bf2e3b94b0b126c74a301a3ccad71b9f
陈珙 <skychensky@outlook.com>
2018-09-04 11:59:36 +0800

    fix expressbuilder

--- Sikiro.DapperLambdaExtension.MsSql.Core/Helper/ExpressionBuilder.cs
+++ Sikiro.DapperLambdaExtension.MsSql.Core/Helper/ExpressionBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -11,34 +12,59 @@ namespace Sikiro.DapperLambdaExtension.MsSql.Core.Helper
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static Expression<Func<T, bool>> Init<T>() { return expression => true; }
-
-        /// <summary>
-        /// 拼接or条件
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="oldExpression"></param>
-        /// <param name="newExpression"></param>
-        /// <returns></returns>
-        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> oldExpression, Expression<Func<T, bool>> newExpression)
+        public static Expression<Func<T, bool>> Init<T>()
         {
-            var parameter = Expression.Parameter(typeof(T));
-            var body = Expression.Or(oldExpression.Body, newExpression.Body);
-            return Expression.Lambda<Func<T, bool>>(body, parameter);
+            return expression => true;
         }
 
-        /// <summary>
-        /// 拼接and条件
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="oldExpression"></param>
-        /// <param name="newExpression"></param>
-        /// <returns></returns>
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> oldExpression, Expression<Func<T, bool>> newExpression)
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first,
+            Expression<Func<T, bool>> second)
         {
-            var parameter = Expression.Parameter(typeof(T));
-            var body = Expression.AndAlso(oldExpression.Body, newExpression.Body);
-            return Expression.Lambda<Func<T, bool>>(body, parameter);
+            return first.Compose(second, Expression.AndAlso);
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first,
+            Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.OrElse);
+        }
+
+        private static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second,
+            Func<Expression, Expression, Expression> merge
+        )
+        {
+            var map = first.Parameters
+                .Select((f, i) => new { f, s = second.Parameters[i] })
+                .ToDictionary(p => p.s, p => p.f);
+
+            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+    }
+    internal class ParameterRebinder : ExpressionVisitor
+    {
+        readonly Dictionary<ParameterExpression, ParameterExpression> _parameterMap;
+
+        ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            _parameterMap = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+        }
+
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map,
+            Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            if (_parameterMap.TryGetValue(p, out var replacement))
+            {
+                p = replacement;
+            }
+
+            return base.VisitParameter(p);
         }
     }
 }
