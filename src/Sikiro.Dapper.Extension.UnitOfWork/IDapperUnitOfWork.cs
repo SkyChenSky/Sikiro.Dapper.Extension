cf08bb0469e60037dd05fadae446de3ee916a62e
陈珙 <287245177@qq.com>
2019-03-24 11:54:13 +0800

    add unit of work project

---
+++ src/Sikiro.Dapper.Extension.UnitOfWork/IDapperUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sikiro.Dapper.Extension.UnitOfWork
+{
+    public interface IDapperUnitOfWork : IDisposable
+    {
+        IDapperUnitOfWork Begin();
+
+        void Commit();
+
+        void Rollback();
+    }
+}
